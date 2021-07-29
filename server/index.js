9862da23f67adbd0710878b39cca5ba4f5df19bf
Sarah Acobera <sacobera1992@go.byuh.edu>
2021-07-29 10:24:41 -1000

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 5500
+
+app.listen(port ,() => { console.log(`Listening on port ${port}`)})
